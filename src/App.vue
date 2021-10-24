9e518fc69fe9f46a2deab1bcb21aa8f7e321f157
oguzhan-derin <oguzhanderinn@gmail.com>
2021-10-24 22:30:20 +0300

    shoecard details

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,161 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+<div>
+  <b-navbar variant="danger" type="light" class="d-flex justify-content-center align-items-center">
+    
+    <b-navbar-brand href="#">Spor Ayakkabı</b-navbar-brand>
+        <b-dropdown id="dropdown-right" right text="Sepet" variant="outline-danger" class="m-2">
+          <template #button-content>
+             &#128722;<span class="right"></span>
+             <b-badge variant="light">{{basketAllItemCount}}</b-badge>
+          </template>
+          
+          <b-dropdown-item v-show="basket.length===0">        
+          Sepetiniz Boş
+        </b-dropdown-item>
+        
+       <b-dropdown-item v-for="(el,i) in basket" :key="i" href="#">
+         <div class="d-flex align-items-center"><b-col>
+                      <img height="100" width="100" :src="el.image" class="cart-img rounded-3"/>
+                    </b-col><b-button class="mx-2" @click="productCal({value: el,type:'-'})" variant="dark" size="sm">-</b-button>
+         {{1}}
+         <b-button class="mx-2" @click="productCal({value: el,type:'+'})" variant="success" size="sm">+</b-button>
+         <b-button @click="productCal({value: el,type:'del'})" variant="danger" size="sm">x</b-button>
+         {{el.title}} - Ara Toplam: {{(el.price * el.count).toFixed(2)}} $</div>
+         
+       </b-dropdown-item> 
+          
+         
+  </b-dropdown> 
+  </b-navbar> 
+
+</div>
+        
+   <b-row class="d-flex justify-content-around">
+      <shoe-card v-for="(el,i) in shoeInfoList " 
+      :key="i" 
+      :info="el"
+      @addShoe="productCal({value:$event,type: 'add'})"      
+      />
+      </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShoeCard from './components/ShoeCard.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {    
+    ShoeCard
+  },
+  data(){
+    return{
+      shoeInfoList:[
+        {
+           category: "WOMEN",
+           title: "Nike React Infinity Run Flyknit ",
+           price: 130,
+           summary:
+            "Kadın Koşu Yol Ayakkabısı",
+           rating: 3,
+           color: "Pink",
+           sizes: [35, 36, 37, 38, 39, 40],           
+           image:require('@/assets/pinkshoes.jpg'),
+
+        },
+        {
+           category: "WOMEN",
+           title: "Nike React Infinity Run Flyknit 2",
+           price: 155,
+           summary:
+            "Kadın Koşu Yol Ayakkabısı",
+           rating: 4,
+           color: "Pink",
+           sizes: [35, 36, 37, 38, 39, 40],           
+           image:require('@/assets/pink2.png'),
+
+        },
+        {
+           category: "MEN",
+           title: "Nike React Infinity Run Flyknit 2 You",
+           price: 185,
+           summary:
+            "Erkek Koşu Yol Ayakkabısı",
+           rating: 5,
+           color: "Blue",
+           sizes: [35, 36, 37, 38, 39, 40],           
+           image:require('@/assets/blueshoes.png'),
+
+        },
+        {
+           category: "MEN",
+           title: "Nike React Infinity Run Flyknit 2 By You",
+           price: 170,
+           summary:
+            "Erkek Koşu Yol Ayakkabısı",
+           rating: 5,
+           color: "Blue",
+           sizes: [35, 36, 37, 38, 39, 40],           
+           image:require('@/assets/blue2.png'),
+
+        },
+  
+      ],
+      basket: []
+   }
+  } ,
+    computed:{
+      basketAllItemCount(){ 
+      let count=0
+      for(let i=0;i<this.basket.length;i++){
+      count+=this.basket[i].count
+      }
+      return count
+      },
+      totalPrice(){
+      let totalPrice=0
+       for(let i=0;i<this.basket.length;i++){
+      totalPrice += this.basket[i].price
+      }      
+      return totalPrice
+      }
+    },
+          
+    
+
+    
+    methods: {
+
+    productCal(val){
+         let index = this.basket.findIndex(el=>el.title===val.value.title)
+
+      switch(val.type){
+          case 'add':
+            if(index>=0){
+              this.basket[index].count++
+            } else{
+          this.basket.unshift({...val.value,count: 1})
+            }
+              break;
+          case '+':
+            this.basket[index].count++
+              break;
+          case '-':
+            if(this.basket[index].count>1){           
+              this.basket[index].count--
+            }else{
+              this.basket.splice(index,1)
+         }
+              break;
+          case 'del':
+              this.basket.splice(index,1)
+              break;
+      default:
+      }
+    }
+   }
 }
 </script>
 
---
+++ src/components/ShoeCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <b-card
+        v-bind:title="info.title"
+        img :src="info.image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="width: 20rem;"
+        class="mb-2" >
+
+        <b-card-img  height="225" width="200" :src="info.image" alt="Image" class="rounded-3 ml-2">
+
+        </b-card-img>
+      
+        <b-card-text>{{info.summary}}</b-card-text>
+
+        <b-row>
+          <div class="sizes">
+            <span>Size:</span>
+               <div class="size-box" v-for="(info,i) in info.size" :key="i">
+                  <input class="rounded" type="button"  :value="info" @click="changeSizes(info)" />
+                </div>
+            </div>
+        </b-row>         
+
+        <b-row class="justify-content-around">
+            <div class="d-flex flex-row align-items-center justify-center">
+                <span v-for="(el,i) in info.rating" 
+                       :key="i" 
+                       class="fa fa-star checked" />
+                <span  v-for="(el,i) in (5-info.rating)" 
+                       :key="'empty'+i" 
+                       class="fa fa-star" />
+             </div>
+                    <b-row class=mt-1>
+                    <b-card-text class="text-left product-price">
+                        $ {{changePrice}}</b-card-text>
+                  </b-row>
+    <button @click="addShoe(info)" type="button" class="btn btn-success">Add To Card
+      <span class="badge badge-light"></span>
+    </button>
+   </b-row>
+  </b-card>
+ </div>
+</template>
+
+<script>
+export default {
+    name:'ShoeCard',
+    props:{
+        info: Object
+    },
+    data(){
+        return{
+         sizes:this.info.sizes[0],
+         comPutedPrice:this.info.price
+        }
+    },
+    computed:{
+    changePrice(){
+      return this.info.price + this.info.price * (this.info.sizes)
+    }
+    },
+
+    methods: {
+        addShoe(val){
+            
+            this.$emit('addShoe',val),
+
+            this.$bvToast.toast(`Ürün Sepete Eklendi`, {
+            title: 'Ürün Eklendi!',
+            autoHideDelay: 2000,
+            appendToast: true,
+            toaster: 'b-toaster-top-right'
+        })
+
+        },
+      changeSizes(val){
+        this.sizes=val
+      }
+}
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.checked {
+  color: orange;
+}
+</style>
